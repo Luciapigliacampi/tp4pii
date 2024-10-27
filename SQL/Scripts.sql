ffe3d7434110cc9c12910372a4cfdaa1683b5807
Lucia Pigliacampi <lucia.pigliacampi90@gmail.com>
2024-10-27 00:24:53 -0300

    Scripts SQL

---
+++ SQL/Scripts.sql
@@ -0,0 +1,17 @@
+select * from rubros;
+
+
+
+insert into productos (nombre,descripcion,precio,imagen)
+values ('Acrilico EQ rojo fuego','Acrilico decorativo x 50 cc',600,'Z:\Lucia\tp4pii\Front\img\acrilico_eq_rojo_fuego.jpg'),
+('Acrilico EQ azul ultramar','Acrilico decorativo x 50 cc',600,'Z:\Lucia\tp4pii\Front\img\acrilico_eq_azul_ceruleo.jpg'),
+('Pincel liner 000','Pincel cerda sintética fina 2 mm',1500,'Z:\Lucia\tp4pii\Front\img\pincel_liner.jpg'),
+('Base de Maquillaje Líquida Maybelline Fit Me Matte + Poreless 118 Light Beige','Cubre imperfecciones, desvanece los poros y controla el brillo para darle a tu rostro un efecto natural e impecable.
+En su nuevo pack, ahora cuenta con FPS 22 y 16 hs de control de brillo','Z:\Lucia\tp4pii\Front\img\base_fit.jpeg'),
+('Delineador de Ojos Líquido Maybelline Hyper Precise All Day Black Matte x 1 g','Tono mate. Larga duración y punta de alta precisión.',20630,'Z:\Lucia\tp4pii\Front\img\delinador_liquido.jpg'),
+('Mod Podge x 100 cc.','Adhesivo ideal para pegar papeles a superficies absorbentes (cartón, madera, yeso, telgopor, etc). Solubles en agua. NO TÓXICO',2500,'Z:\Lucia\tp4pii\Front\img\mod_podge.jpg'),
+('Maybelline The Colossal Big Shot Máscara De Pestañas Waterproof','El cepillo Big Shot tiene cerdas onduladas únicas que mueven las pestañas de lado a lado, mientras que la fórmula con colágeno aporta un volumen intenso en una sola pasada. Pestañas impactantes de raí­z a puntas y a prueba de agua.',23000,'Z:\Lucia\tp4pii\Front\img\mascara_pestanas.jpg'),
+('Bolsa camiseta 40X50','Bolsa camise de alta densidad 40x50 cm x 100 unidades',2630.03,'Z:\Lucia\tp4pii\Front\img\bolsa_camiseta.jpg'),
+('','',,''),
+('','',,''),
+('','',,''),;
